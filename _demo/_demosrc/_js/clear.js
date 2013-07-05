e5b20f19aacec597afa8fa1496e75155c7f06cf0
Jeff Vlahos <jeff@upstatement.com>
2013-07-05 18:10:08 -0400

    JS cleanup

--- _demo/_demosrc/_js/clear.js
+++ _demo/_demosrc/_js/clear.js
@@ -1,3 +1,7 @@
+// $(function() {
+//     FastClick.attach(document.body);
+// });
+
 $(document).ready(function(){
 
 	//Sortable. This works if all the other events are turned off
@@ -24,6 +28,9 @@ $(document).ready(function(){
 
 	//The spacer is placed (and maintained) between incomplete and complete items
 	var spacer = $('#spacer');
+	var hanger = $('.hanger');
+	var newItemTop = $('body').children('#new-item-top');
+	var bodyClasses = $('body, .mod');
 
 	//These booleans get set and unset throughout functions
 	//So that we can ensure only one gestural reaction can happen at a time
@@ -44,18 +51,22 @@ $(document).ready(function(){
 	
 	// Pull to Create Task - Drag Events
 	$(document).hammer({drag_min_distance: 0}).on('drag', '.mod', function(event){
-		if ($(window).scrollTop() <= 0) {
-			if (event.gesture.deltaY > 67 ) {
-				$('#new-item-top').find('input').val('Release to Create Item');
-				$('body').css('margin-top', ((event.gesture.deltaY) - 67)+"px");
-			} else {
-				$('#new-item-top').css('height', event.gesture.deltaY+"px");
-				$('#new-item-top .item').css('background-color', 'hsl(0, 100%, '+((event.gesture.deltaY)-10)+'%)');
-				$('#new-item-top .item').css('border-color', 'hsl(0, 100%, '+((event.gesture.deltaY)-30)+'%)');
-				$('#new-item-top').find('input').val('Pull to Create Task');
-				$('#new-item-top').css('-webkit-transform', 'rotateX('+(90-(((event.gesture.deltaY)/67)*90))+'deg)');
+			if (event.gesture.deltaY > 67) {
+				event.gesture.deltaY = 67;
 			}
-		}
+			if (event.gesture.deltaY > 67 ) {
+				newItemTop.find('input').val('Release to Create Item');
+				$('body').css('margin-top', ((event.gesture.deltaY) - 67)+"px");
+				newItemTop.css('height', "67px");
+			} else {
+				//$(window).scrollTop(0);
+				newItemTop.css('height', event.gesture.deltaY+"px");
+				newItemTop.find('.item')
+					.css('background-color', 'hsl(0, 100%, '+((event.gesture.deltaY)-10)+'%)')
+					.css('border-color', 'hsl(0, 100%, '+((event.gesture.deltaY)-30)+'%)');
+				newItemTop.find('input').val('Pull to Create Task');
+				newItemTop.css('-webkit-transform', 'rotateX('+(90-(((event.gesture.deltaY)/67)*90))+'deg)');
+			}
 		if (event.gesture.deltaY > 20 || event.gesture.deltaY < -20) {
 			scroll = true;
 		}
@@ -65,7 +76,7 @@ $(document).ready(function(){
 	$(document).hammer({drag_min_distance: 0}).on('dragend', '.mod', function(event){
 		if (event.gesture.deltaY > 67 ) {
 			$('body').addClass('slide-back').css('margin-top','0px');
-			$('#new-item-top').css('height', "0px");
+			newItemTop.css('height', "0px");
 			$('.mod').prepend(itemModTemplate.clone());
 			updateColors();
 			$('.item-mod.new input').focus();
@@ -74,12 +85,14 @@ $(document).ready(function(){
 				$('body').removeClass('slide-back');
 			}, 200);
 		} else {
-			$('#new-item-top').addClass('animate-back');
-			$('#new-item-top').css('height', "0px");
-			$('#new-item-top').css('-webkit-transform', 'rotateX(90deg)');
+			newItemTop
+				.addClass('animate-back')
+				.css('height', "0px")
+				.css('-webkit-transform', 'rotateX(90deg)');
 			setTimeout(function() {
-				$('#new-item-top').removeClass('animate-back');
-				$('#new-item-top').attr('style','');
+				newItemTop
+					.removeClass('animate-back')
+					.attr('style','');
 			}, 100);
 		}
 		scroll = false;
@@ -109,22 +122,25 @@ $(document).ready(function(){
 		if (!v.$itemMod.hasClass('done') && !v.$itemMod.hasClass('was-done')) {
 			//If scroll enters the done position
 			if ( dragX > 55 ) {
-				v.$itemMod.css('background-position-x', (dragX-55)+"px");
-				v.$itemMod.addClass('check');
+				v.$itemMod
+					.css('background-position-x', (dragX-55)+"px")
+					.addClass('check');
 			//If scroll enters the clear position
 			} else if ( dragX < -55 ) {
 				v.$itemMod.css('background-position-x', (dragX+55)+"px");
 			//Anywhere in between
 			} else {
-				v.$itemMod.css('background-position-x', '0px');
-				v.$itemMod.removeClass('check');
+				v.$itemMod
+					.css('background-position-x', '0px')
+					.removeClass('check');
 			}
 		//If item is done
 		} else {
 			//If scroll enters the un-do position
 			if ( dragX > 55 ) {
-				v.$itemMod.css('background-position-x', (dragX-55)+"px");
-				v.$itemMod.removeClass('done');
+				v.$itemMod
+					.css('background-position-x', (dragX-55)+"px")
+					.removeClass('done');
 				v.$item
 					.css('background-color', 'hsl(45,100%, 50%);')
 					.css('border-top-color', 'hsl(45,100%, 70%);')
@@ -135,8 +151,9 @@ $(document).ready(function(){
 				v.$itemMod.css('background-position-x', (dragX+55)+"px");
 			//Anywhere in between
 			} else {
-				v.$itemMod.css('background-position-x', '0px');
-				v.$itemMod.addClass('done');
+				v.$itemMod
+					.css('background-position-x', '0px')
+					.addClass('done');
 				//v.$itemInput.prop('disabled', true);
 			}
 		}
@@ -169,22 +186,32 @@ $(document).ready(function(){
 			//And release point is in the 'done' zone
 			if ( releasePoint > 55 ) {
 				itemMotion = true;
-				v.$item.addClass('slide-back').css('margin-left','0px');
+				v.$item
+					.addClass('slide-back')
+					.css('margin-left','0px');
 				afterCount = v.$itemMod.nextAll().not('.done').length;
 				v.$itemMod.css('background-position-x', ((-60)-($(this).scrollLeft()))+"px");
 				//Delay for item to momentum-scroll back, then do the move
 			    setTimeout( function(){
 			    	v.$item.removeClass('slide-back');
-			    	v.$itemMod.css('background-image','none');
-					v.$itemMod.addClass('done').removeClass('check');
+			    	v.$itemMod
+			    		.css('background-image','none')
+						.addClass('done')
+						.removeClass('check');
 					v.$itemModClone = v.$itemMod.clone();
-			    	v.$itemModClone.insertAfter(v.$itemMod).addClass('shrink');
+			    	v.$itemModClone
+			    		.insertAfter(v.$itemMod)
+			    		.addClass('shrink');
 					spacer.addClass('make-space');
-					v.$itemMod.addClass('remove').css('-webkit-transform', 'translate(0px,'+((afterCount-1)*67)+'px)');
+					v.$itemMod
+						.addClass('remove')
+						.css('-webkit-transform', 'translate(0px,'+((afterCount-1)*67)+'px)');
 				},200 );
 				//Delay for another 0.5s for item move to occur, then clean up the leftovers
 				setTimeout(function() {
-    				v.$itemModClone.insertAfter(spacer).removeClass('shrink');
+    				v.$itemModClone
+    					.insertAfter(spacer)
+    					.removeClass('shrink');
     				v.$itemMod.hide().remove();
     				spacer.removeClass('make-space');
     				v.$itemMod.css('background-image','url("clear.png")');
@@ -203,7 +230,9 @@ $(document).ready(function(){
 			//And release point is in the un-do position
 			if ( releasePoint > 55 ) {
 				itemMotion = true;
-				v.$item.addClass('slide-back').css('margin-left','0px');
+				v.$item
+					.addClass('slide-back')
+					.css('margin-left','0px');
 				beforeCount = v.$itemMod.prevAll('.done').length;
 				v.$itemMod.css('background-position-x', ((-60)-($(this).scrollLeft()))+"px");
 			    setTimeout( function(){
@@ -211,8 +240,12 @@ $(document).ready(function(){
 					v.$itemMod.removeClass('done was-done check');
 					spacer.addClass('make-space');
 					v.$itemModClone = v.$itemMod.clone();
-			    	v.$itemModClone.insertAfter(v.$itemMod).addClass('shrink');
-					v.$itemMod.addClass('remove').css('-webkit-transform', 'translate(0px,'+(-(beforeCount+1)*67)+'px)');
+			    	v.$itemModClone
+			    		.insertAfter(v.$itemMod)
+			    		.addClass('shrink');
+					v.$itemMod
+						.addClass('remove')
+						.css('-webkit-transform', 'translate(0px,'+(-(beforeCount+1)*67)+'px)');
 				}, 200);
 				
 				setTimeout(function() {
@@ -235,7 +268,9 @@ $(document).ready(function(){
 		if ( releasePoint < -55 ) {
 			itemMotion = true;
 			v.$itemMod.css('background-image', 'none');
-			v.$item.addClass('slide-out').css('margin-left','-320px');
+			v.$item
+				.addClass('slide-out')
+				.css('margin-left','-320px');
 			v.$itemMod.addClass('shrink-after gone');
 			setTimeout(function() {
 				v.$itemMod.remove();
@@ -249,15 +284,16 @@ $(document).ready(function(){
 
 	function updateColors(){
 		var hue;
-		var items = $('#mod .item-mod').not('.done').not('.check');
+		var items = $('#mod .item-mod').not('.done').not('.check').not('.remove');
 		var itemsCount = items.length;
+		console.log(items);
 		if (itemsCount < 5) {  itemsCount = 5; }
 		items.each(function( index ){
-			hue = (45/itemsCount) * (index+1);
+			hue = (30/itemsCount) * (index);
 			$(this).children('.item')
 				.css('background-color', 'hsl('+hue+',100%, 50%);')
 				.css('border-top-color', 'hsl('+hue+',100%, 70%);')
-				.css('border-bottom-color', 'hsl('+hue+',100%, 30%);')
+				.css('border-bottom-color', 'hsl('+hue+',100%, 30%);');
 		});
 	}
 
@@ -265,63 +301,71 @@ $(document).ready(function(){
 	var itemModTemplate = $('#item-mod-template .item-mod');
 
 	// Space below items that triggers a new item to be created
-	$(document).hammer().on('tap', '#new-item-trigger', function(){
-		if ( focus == true ) { return; }
-		$(this).find('.hanger').css('-webkit-transform','rotateX(0deg)');
-		$(this).find('.hanger').find('.item').css('background-color', 'hsl(45, 100%, 50%)');
-		setTimeout(function(){
-			$('.hanger').addClass('no-transition');
-			$('.hanger').attr('style','');
-			$('.hanger .item').attr('style','');
-			itemModTemplate.clone().insertBefore(spacer);
-			$('.mod .item-mod.new input').focus();
-			$('.mod .item-mod.new').removeClass('new');
-			$('.hanger').removeClass('no-transition');
-			focus = true;
-			updateColors();
-		}, 400);
+	// $(document).hammer().on('tap', '#new-item-trigger', function(){
+	// 	if ( focus == true ) { return; }
+	// 	$(this).find('.hanger').css('-webkit-transform','rotateX(0deg)');
+	// 	$(this).find('.hanger').find('.item').css('background-color', 'hsl(45, 100%, 50%)');
+	// 	setTimeout(function(){
+	// 		var hanger = $('.hanger');
+	// 		hanger
+	// 			.addClass('no-transition')
+	// 			.attr('style','')
+	// 			.find('.item').attr('style','');
+	// 		itemModTemplate.clone().insertBefore(spacer);
+	// 		$('.mod .item-mod.new input')
+	// 			.focus()
+	// 			.closest('.item-mod.new').removeClass('new');
+	// 		hanger.removeClass('no-transition');
+	// 		focus = true;
+	// 		updateColors();
+	// 	}, 400);
 		
-	});
+	// });
 
 	// A tap on an item triggers focus on the input within that item
 	$(document).hammer().on('tap', '.item', function(){
 		if ( focus == true || $(this).parent().hasClass('done') ) { return; }
 		$(this).find('input').removeAttr("disabled").focus();
 		$(this).closest('.item-mod').addClass('focus');
-		$('.mod, body').addClass('focus');
+		bodyClasses.addClass('focus');
 	});
 
 	// Whenever focus on an input occurs, add some reference classes
 	// So we know that an item is currently being edited
 	$(document).on('focus','input',function(){
-		$('.mod, body').addClass('focus');
+		bodyClasses.addClass('focus');
 		$(this).closest('.item-mod').addClass('focus');
 	});
 
 	// Whenever an item is done being edited, close everything up.
 	// If the item has no contents, trash it.
 	$(document).on('blur','input', function(){
+		var hanger = $('.hanger');
 		var itemMod = $(this).closest('.item-mod');
-		$('.hanger').css('-webkit-transform','rotateX(-90deg)');
+		hanger.css('-webkit-transform','rotateX(-90deg)');
 		$(this).attr("disabled", "disabled");
-		$('.mod, body').removeClass('focus');
+		bodyClasses.removeClass('focus');
 		itemMod.removeClass('focus');
-		$('.hanger').attr('style','');
+		hanger.attr('style','');
 		var value = $(this).val();
 		if (value == "" || value == 0) {
-			itemMod.css('-webkit-overflow-scrolling','none').css('background-image', 'none');
-			itemMod.addClass('transition');
-			itemMod.find('.item').css('left', releasePoint+'px').css('position','absolute');
-			itemMod.find('.item').css('left','-320px');
-			itemMod.addClass('shrink-after gone');
+			itemMod
+				.css('-webkit-overflow-scrolling','none')
+				.css('background-image', 'none')
+				.addClass('transition');
+			itemMod.find('.item')
+				.css('left', releasePoint+'px')
+				.css('position','absolute')
+				.css('left','-320px')
+				.addClass('shrink-after gone');
 			setTimeout(function() {
 				itemMod.remove();
-				$('.hanger').attr('style','');
+				hanger.attr('style','');
 				focus = false;
 			}, 400);
 		} else {
 			setTimeout(function() {
-				$('.hanger').attr('style','');
+				hanger.attr('style','');
 				focus = false;
 			}, 400);
 		}
@@ -343,14 +387,14 @@ $(document).ready(function(){
 	        (keycode > 47 && keycode < 58)   || // number keys
 	        (keycode > 64 && keycode < 91)   || // letter keys
 	        (keycode > 95 && keycode < 112) // numpad keys
-		if (!$('.mod, body').hasClass('focus') && valid) {
+		if (!bodyClasses.hasClass('focus') && valid) {
 			$('#new-item-trigger').trigger('tap');
 			wait = true;
 			setTimeout(function() {
 				$('.item-mod.focus').find('.item').find('input').val(String.fromCharCode(e.which));
 				wait = false;
 			}, 400);
-		} else if ($('.mod, body').hasClass('focus') && e.keyCode == 27) {
+		} else if (bodyClasses.hasClass('focus') && e.keyCode == 27) {
 			$('.item-mod.focus').find('input').val('').blur();
 		}
 	});
