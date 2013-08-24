c1a373a190340ef210d79421fbba43a8a5ee6406
Jeff Vlahos <jeff@upstatement.com>
2013-08-24 10:25:13 -0400

    analytics

--- _demo/index.php
+++ _demo/index.php
@@ -61,5 +61,14 @@
 	<!--<script src="_js/libs/jquery-ui-1.10.3.custom.min.js"></script>-->
 	<script src="_js/clear.js"></script>
 
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-22169597-2', 'jeffvlahos.com');
+  ga('send', 'pageview');
+	</script>
+
 </body>
 </html>
